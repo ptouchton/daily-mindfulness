3cb971f7037f88e79cb4aa5071b7a54b7267054a
Paul Touchton <p_touchton@yahoo.com>
2020-06-29 21:30:27 -0700

    adding some observables for the logged in user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
 
-@Component({
+import { AuthService } from './services/auth.service';
+import { Router, Event, NavigationError, NavigationStart, NavigationEnd, NavigationCancel } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { Subject, EMPTY } from 'rxjs';
+
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'daily-mindfulness';
+
+  pageTitle = 'Daily Mindfulness';
+  loading = true;
+
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+
+  get isLoggedIn(): boolean {
+    return this.authService.loggedIn;
+  }
+
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  userName$ = this.authService.userProfile$.pipe(
+    catchError(err => {
+      this.errorMessageSubject.next(err);
+      return EMPTY;
+    })
+  );
+
+
+  // displayMessages(): void {
+  //   this.router.navigate([{ outlets: {popup: ['messages']}}]);
+  //   this.messageService.isDisplayed = true;
+  // }
+
+  // hideMessages(): void {
+  //   this.router.navigate([{ outlets: {popup: null}}]);
+  //   this.messageService.isDisplayed = false;
+  // }
+  checkRouterEvent(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+
+  logOut(): void {
+    this.authService.logout();
+    console.log('Log out');
+    this.router.navigateByUrl('/welcome');
+  }
 }
